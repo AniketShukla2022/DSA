62bd8c845f2a429c55e6e62ce9dc59a3a9f820e8
Aniket Shukla <54362279+AniketShukla2022@users.noreply.github.com>
2022-09-08 23:43:54 +0530

    LeetCode-442

---
+++ FindAllDuplicatesInArray.cpp
@@ -0,0 +1,12 @@
+vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        if (nums.size() ==1 )
+            return {};
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[abs(nums[i])-1] < 0)
+                ans.push_back(abs(nums[i]));
+            else
+                nums[abs(nums[i])-1] = -1*nums[abs(nums[i])-1];
+        }
+        return ans;
+    }
