30da5aa687dc64baccee76305af29609b8bfd366
Aniket Shukla <54362279+AniketShukla2022@users.noreply.github.com>
2022-09-08 23:52:48 +0530

    CodeStudio

---
+++ IntersectionOfTwoArrays.cpp
@@ -0,0 +1,18 @@
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+	// Write your code here.
+    vector<int> intersection;
+    int i=0, j=0;
+    while (i<n && j<m) {
+        if(arr1[i] == arr2[j]) {
+            intersection.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+        else if(arr1[i] < arr2[j])
+            i++;
+        else
+            j++;
+    }
+    return intersection;
+}
