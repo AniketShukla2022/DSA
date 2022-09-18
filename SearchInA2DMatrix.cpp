d962c9e14f31c9345ab951d97c827ba3c2fde92b
Aniket Shukla <54362279+AniketShukla2022@users.noreply.github.com>
2022-09-18 14:45:52 +0530

    LeetCode-74-BinarySearch-T.C-O(Log(m*n))

---
+++ SearchInA2DMatrix.cpp
@@ -0,0 +1,19 @@
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int s = 0;
+        int e = m*n-1;
+        int mid = s+(e-s)/2;
+        
+        while (s<=e) {
+            if (matrix[mid/n][mid%n] == target)
+                return true;
+            if (matrix[mid/n][mid%n] > target)
+                e = mid-1;
+            else
+                s = mid+1;
+            mid = s+(e-s)/2;
+        }
+        return false;
+    }
