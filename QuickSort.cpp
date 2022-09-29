0ab9cd0c40e39f07e59dffbf3624607923608d0a
Aniket Shukla <54362279+AniketShukla2022@users.noreply.github.com>
2022-09-29 21:56:39 +0530

    Sorting Technique - 5

---
+++ QuickSort.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h> 
+int partition(vector<int>& arr, int s, int e) {
+    int pivot = arr[s];
+    int cnt = 0;
+    for (int i=s+1; i<=e; i++) {
+        if (arr[i]<=pivot) {
+            cnt++;
+        }
+    }
+    int pivotIdx = s+cnt;
+    swap(arr[pivotIdx],arr[s]);
+    int i=s, j=e;
+    while (i<pivotIdx && j>pivotIdx) {
+        while (arr[i]<=pivot)
+            i++;
+        while (arr[j]>pivot)
+            j--;
+        if (i<pivotIdx && j>pivotIdx) {
+            swap(arr[i++],arr[j--]);
+        }
+    }
+    return pivotIdx;
+}
+void solve(vector<int>& arr, int s, int e) {
+    if (s>=e) {
+        return;
+    }
+    int p = partition(arr,s,e);
+    solve(arr,s,p-1);
+    solve(arr,p+1,e);
+}
+vector<int> quickSort(vector<int> arr)
+{
+    int n = arr.size();
+    solve(arr,0,n-1);
+    return arr;
+}
