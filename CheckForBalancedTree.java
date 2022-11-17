639978a9556df4a1979c47a545c010ac5e403d33
Aniket Shukla <54362279+AniketShukla2022@users.noreply.github.com>
2022-11-17 23:18:10 +0530

    LeetCode

---
+++ CheckForBalancedTree.java
@@ -0,0 +1,18 @@
+class Solution {
+    private int getHeight(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        return Math.max(left,right) + 1;
+    }
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        if (Math.abs(leftHeight-rightHeight) > 1)
+            return false;
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+}
